f20b211127e9aef24fafab60501703e292cd1d8e
David Hugh-Jones <davidhughjones@gmail.com>
2018-03-09 18:09:22 +0000

    tweak names

--- gather-data.R
+++ gather-data.R
@@ -6,7 +6,6 @@ result <- list.files(pattern="*.csv", path = "shared-data", full.names = TRUE) %
       purrr::map_df(~readr::read_csv(., na="n/a", 
             col_types = readr::cols(S3method="c", generic="c")
             ))
-readr::write_csv(result, path = "pkg_data_all.csv")
 
 rcheology <- as.data.frame(result)
 usethis::use_data(rcheology)
