f0aed2d5dca5cc65aa6d87089a10da7f652ac2a0
David Hugh-Jones <davidhughjones@gmail.com>
2018-03-09 22:57:49 +0000

    new version of data

--- gather-data.R
+++ gather-data.R
@@ -2,10 +2,8 @@
 container <- readline("Enter the name of the container: ")
 system2("docker", c("cp", paste0(container, ":", "/rcheology/docker-data/."), "docker-data"))
   
-rcheology <- list.files(pattern="*.csv", path = "shared-data", full.names = TRUE) %>% 
-      purrr::map_df(~readr::read_csv(., na="n/a", 
-            col_types = readr::cols(S3method="c", generic="c")
-            ))
+rcheology <- list.files(pattern="*.csv", path = "docker-data", full.names = TRUE) %>% 
+      purrr::map_df(~readr::read_csv(., na="n/a", col_types = readr::cols(generic="c")))
 
 rcheology <- as.data.frame(rcheology)
 usethis::use_data(rcheology, overwrite = TRUE)
