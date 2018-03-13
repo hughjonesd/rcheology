3034b7e75f2f0d7b2da225bb61448328cd6fd03f
David Hugh-Jones <davidhughjones@gmail.com>
2018-03-13 18:21:32 +0000

    tweak app

--- app/app.R
+++ app/app.R
@@ -8,26 +8,62 @@
 #
 
 library(shiny)
-
+library(DT)
 
 load("rcheology-app-data.RData")
+
+kw_opts <- list(
+      debug  = list(
+        global = "", 
+        columns = c("debug", "base", rep("", ncol(rch_summary) - 2))
+      ),
+      order  = list(
+        global = "",
+        columns = c("order", "base", rep("", ncol(rch_summary) - 2))
+      ),
+      hasName = list(
+        global = "hasName", 
+        columns = rep("", ncol(rch_summary))
+      )
+)
+keywords <- names(kw_opts)
 
 ui <- fluidPage(
   titlePanel("Base R Functions from 3.0.1 to 3.4.3"),
   sidebarLayout(
-    sidebarPanel("Created using the ", a(href="https://github.com/hughjonesd/rcheology", "rcheology"), " package."),
+    sidebarPanel(
+      "Created using the ", a(href = "https://github.com/hughjonesd/rcheology", "rcheology"), 
+      " package.", " Try: ", lapply(keywords, function (kw) actionLink(kw, kw))
+    ),
+      
     mainPanel(
-      dataTableOutput("rcheology")
+      dataTableOutput("rcheology_DT")
     )
   )
 )
 
 server <- function(input, output) {
-   output$rcheology <- renderDataTable(rch_summary, 
-        options = list(
-          pageLength = 20,
+   output$rcheology_DT <- DT::renderDataTable(
+        rch_summary, 
+        rownames = FALSE,
+        filter   = "top",
+        options  = list(
+          pageLength = 10,
+          search     = list(regex = TRUE, caseInsensitive = FALSE),
           order      = c(0, "asc")
         ))
+   
+   dt_proxy <- DT::dataTableProxy("rcheology_DT")
+   
+   lapply(keywords, function (kw) {
+     observeEvent(input[[kw]], {
+       DT::updateSearch(
+              dt_proxy, 
+              keywords = kw_opts[[kw]]
+            )
+     })
+   })
+   
 }
 
 # Run the application 
