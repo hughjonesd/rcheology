49f8c4f703ad86d8b008b5e0ff715de857a0c644
David Hugh-Jones <davidhughjones@gmail.com>
2018-03-13 17:22:28 +0000

    separate making data for app

--- app/app.R
+++ app/app.R
@@ -9,35 +9,8 @@
 
 library(shiny)
 
-REBUILD <- TRUE
 
-make_range <- function (versions) {
-  lv <- length(all_versions)
-  versions <- sort(as.package_version(unique(versions)))
-  there <- all_versions %in% versions
-  stop_start <- c(as.numeric(there[1]), diff(there))
-  starts <- which(stop_start == 1)
-  stops  <- which(stop_start == -1) - 1
-  if (stop_start[lv] == 0 && there[lv]) stops <- c(stops, lv) 
-  start_v <- all_versions[starts]
-  stop_v  <- all_versions[stops]
-  stop_v <- ifelse(start_v == stop_v, "", paste0("-", stop_v))
-  ranges <- paste0(start_v, stop_v, sep = "", collapse = "; ")
-  
-  ranges
-}
-
-if (REBUILD) {
-  library(rcheology)
-  rch_summary <- rcheology %>% 
-        group_by(name, package, args) %>% 
-        summarize(versions = make_range(Rversion))
-  save(rcheology, rch_summary, file = "rcheology-app-data.RData")
-}
 load("rcheology-app-data.RData")
-
-all_versions <- sort(as.package_version(unique(rcheology$Rversion)))
-
 
 ui <- fluidPage(
   titlePanel("Base R Functions from 3.0.1 to 3.4.3"),
