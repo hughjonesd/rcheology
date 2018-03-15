190e78b057a0402ace349fbfeb2c9a046d0d76b5
David Hugh-Jones <davidhughjones@gmail.com>
2018-03-15 20:08:23 +0000

    new fn_changed package

--- R/rcheology.R
+++ R/rcheology.R
@@ -14,3 +14,43 @@
 #' 
 #' @name rcheology 
 NULL
+
+#' Check if a core R function changed between R versions
+#'
+#' @param fn Character name of a function in a core R package.
+#' @param from Minimum R version (optional).
+#' @param to Maximum R version (optional).
+#' @param package Name of the package (optional).
+#' 
+#' @return 0 if there was no change. 1 if the function's arguments changed.
+#'   2 if the function was not present in all versions. If the function can't
+#'   be found or exists in multiple packages, throws an error.
+#' @export
+#'
+#' @examples
+#' fn_changed("debugonce")
+#' fn_changed("debugonce", "3.4.0", "3.4.3")
+#' fn_changed("debugonce", "3.3.0", "3.4.3")
+fn_changed <- function (fn, from = NULL, to = NULL, package = NULL) {
+  vns <- as.package_version(rcheology$Rversion)
+  relevant_vns <- unique(vns)
+  range <- rcheology$name == fn
+  if (! is.null(package)) range <- range & rcheology$package == package
+  if (! is.null(from))    {
+    from <- as.package_version(from)
+    range <- range & vns >= from
+    relevant_vns <- relevant_vns[relevant_vns >= from]
+  }
+  if (! is.null(to)) {
+    to   <- as.package_version(to)
+    range <- range & vns <= to
+    relevant_vns <- relevant_vns[relevant_vns <= to]
+  }
+  fns <- rcheology[range, , drop = FALSE]
+  args <- fns$args
+  if (length(unique(fns$package)) > 1) stop("Multiple functions found with that name")
+  if (nrow(fns) == 0) stop("Couldn't find function of those versions")
+  if (nrow(fns) < length(relevant_vns)) return(2)
+  if (length(unique(args)) > 1) return(1)
+  return(0)
+}
