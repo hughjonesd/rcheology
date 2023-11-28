94be6c18ab5c8b7903dbdb49e9286d9eee5f3a2a
David Hugh-Jones <davidhughjones@gmail.com>
2023-11-28 13:41:17 +0000

    Change RARGS for 0.60 and 0.61 to avoid --no-restore causing argument parse failure

--- guest-run-r-versions.sh
+++ guest-run-r-versions.sh
@@ -13,7 +13,8 @@ for RVERSION in /opt/R/*; do
   RERR="errors/error-$RBASE.txt"
   RARGS="--no-restore --no-save -nosave"
   case $RBASE in
-    0.49 | 0.50-a1 | 0.50-a4 ) RARGS=""
+    0.49 | 0.50-a1 | 0.50-a4 ) RARGS="";;
+    0.60* | 0.61* ) RARGS="-nosave";;
   esac
   RV=$RVERSION $RBIN $RARGS < guest-list-objects.R 1>$RERR 
 done 
