4ee1cb1c293ffc72a94e0cad2cf9219a657660b0
David Hugh-Jones <davidhughjones@gmail.com>
2023-12-01 09:06:16 +0000

    Fixes to app data

--- make-app-data.R
+++ make-app-data.R
@@ -5,18 +5,19 @@ all_versions <- sort(as.package_version(unique(rcheology$Rversion)))
 
 
 make_doc_anchors <- function (name, package, versions) {
-  # rdocumentation.org doesn't have 3.2.0
-  rdoc_versions <- ifelse(versions == '3.2.0', '3.2.1', as.character(versions))
+  r_help_versions <- sub("(1\\.[0-4])\\.0", "\\1", versions)
+  r_help_versions <- sub("(0\\.\\d+)\\.0", "\\1", r_help_versions)
+  
   url <- utils::URLencode(sprintf("https://hughjonesd.github.io/r-help/%s/%s/%s.html",
-                                  rdoc_versions, package, name))
-  # url <- utils::URLencode(sprintf("https://www.rdocumentation.org/packages/%s/versions/%s/topics/%s", 
-  #       package, rdoc_versions, name))
+                                  r_help_versions, package, name))
+  
   versions <- as.character(versions)
   anchors <- paste0("<a href='", url, "' target='_blank'>", versions, "</a>")
         
   anchors
 }
 
+# Makes ranges which may have gaps, e.g. 0.60-0.61; 1.0-1.2.0 
 make_range <- function (name, package, versions) {
   my_doc_anchors <- function (versions) make_doc_anchors(name, package, versions)
 
@@ -26,7 +27,8 @@ make_range <- function (name, package, versions) {
   stop_start <- c(as.numeric(there[1]), diff(there))
   starts <- which(stop_start == 1)
   stops  <- which(stop_start == -1) - 1
-  if (stop_start[lv] == 0 && there[lv]) stops <- c(stops, lv) 
+  if (stop_start[lv] == 0 && there[lv]) stops <- c(stops, lv)
+  
   start_v <- all_versions[starts]
   stop_v  <- all_versions[stops]
   stop_v <- ifelse(start_v == stop_v, "", paste0("-", my_doc_anchors(stop_v)))
@@ -41,15 +43,15 @@ make_range <- function (name, package, versions) {
 
 n_all_versions <- length(unique(rcheology$Rversion))
 
-rch_summary <- rcheology %>% 
+rch_summary <- rcheology |> 
       mutate(
         # for DT search box to display as an option:
         package = as.factor(package),
         type = as.factor(type),
         class = as.factor(class),
         priority = as.factor(priority),
-      ) %>%
-      group_by(name, package, args, type, class, exported) %>% 
+      ) |>
+      group_by(name, package, args, type, class, exported) |> 
       summarize(
         hidden          = hidden[1],
         versions        = make_range(name[1], package[1], Rversion), 
