42071c29cdddb1fca9713477ec8a130d3144b450
David Hugh-Jones <davidhughjones@gmail.com>
2024-06-25 07:53:25 +0100

    Use usethis for github release

--- post-acceptance.sh
+++ post-acceptance.sh
@@ -10,13 +10,6 @@ git tag -m "Version $VERSION on CRAN" -a v$VERSION $CRANCOMMIT
 git push --tags
 rm CRAN-SUBMISSION
 
-# reset revdepcheck
 Rscript -e 'revdepcheck::revdep_reset()'
+Rscript -e 'usethis::use_github_release()'
 
-VERSION=$VERSION Rscript -e '
-version = Sys.getenv("VERSION")
-gh::gh("POST /repos/hughjonesd/rcheology/releases", 
-       name = version,
-       tag_name = paste0("v", version),
-       body = sprintf("Version %s on CRAN", version))
-'
